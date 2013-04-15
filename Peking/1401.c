4b80cc954ab0c0efb740f19c284e288a61eec188
Wen Jia Liu <wj32.64@gmail.com>
2013-04-15 20:01:01 +1000

    improved POJ 1401

--- Peking/1401.c
+++ Peking/1401.c
@@ -7,23 +7,6 @@ int mult(int n, int k)
     return n / k;
 }
 
-// Returns b^e.
-int powi(int b, int e)
-{
-    int r = 1;
-    
-    while (e)
-    {
-        if (e & 1)
-            r *= b;
-        
-        e /= 2;
-        b *= b;
-    }
-    
-    return r;
-}
-
 // Sum of d(m, p) for m=1,...,n,
 // where d=d(m, p) is largest integer such that p^d divides m.
 // p must be prime.
@@ -31,21 +14,11 @@ int powi(int b, int e)
 int xf(int n, int p, int sd)
 {
     int result = 0;
-    int d = sd;
-    int pd = powi(p, d);
-    int k;
-    int j;
+    int d;
+    int pd = p;
     
-    j = mult(n, pd);
-    result = j * d;
-    
-    while (--d >= 1)
-    {
-        pd /= p;
-        k = mult(n, pd);
-        result += (k - j) * d;
-        j = k;
-    }
+    for (d = 1; d <= sd; d++, pd *= p)
+        result += n / pd;
     
     return result;
 }
@@ -54,13 +27,12 @@ int zf(int n)
 {
     // 2^30 > 10^9
     // 5^13 > 10^9
-    int d2 = xf(n, 2, 30);
+    //int d2 = xf(n, 2, 30);
     int d5 = xf(n, 5, 13);
     
-    if (d2 < d5)
-        return d2;
-    else
-        return d5;
+    // note d2 is always larger than d5
+    
+    return d5;
 }
 
 int main(int argc, char *argv[])
