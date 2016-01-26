421ceab110a5fde9d260ca5522b56acb60010e7f
Wen Jia Liu <wj32.64@gmail.com>
2016-01-26 16:04:12 +1100

    Add some simulations

--- 100 Coins.fsx
+++ 100 Coins.fsx
@@ -32,5 +32,39 @@ let histogram f n =
       counts.Add(value, 1) |> ignore
   counts |> Seq.map (|KeyValue|) |> Map.ofSeq
 
+let expectation hist =
+  hist
+  |> Map.toSeq
+  |> Seq.fold
+    (fun (totalSum, totalCount) (key, count) -> (totalSum + key * count, totalCount + count))
+    (0, 0)
+  |> (fun (totalSum, totalCount) -> float totalSum / float totalCount)
+
 let dist = histogram (fun () -> experiment 100) 1000000
 let distCounts = dist |> Map.toSeq |> Seq.map snd |> Seq.toList
+
+// Other random stuff
+
+module OtherExperiments =
+  let rollWithBust bust reroll =
+    let rec loop acc =
+      let acc' = acc + random.Next(1, 7)
+      if acc' >= bust then
+        0
+      else
+        if reroll acc' then
+          loop acc'
+        else
+          acc'
+    loop 0
+  let breakStickTriangle () =
+    let (x, y) = (random.NextDouble(), random.NextDouble())
+    let (x, y) = if x < y then (x, y) else (y, x)
+    let (a, b, c) = (x, y - x, 1. - y)
+    let success =
+      match a, b, c with
+      | large, small1, small2 when a >= b && a >= c -> small1 + small2 >= large
+      | small1, large, small2 when b >= a && b >= c -> small1 + small2 >= large
+      | small1, small2, large when c >= a && c >= b -> small1 + small2 >= large
+      | _ -> failwith "Invalid state"
+    if success then 1 else 0
